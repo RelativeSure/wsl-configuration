6a5b707373507742f9f1d80b8d8798d3b414a696
Rasmus <67839982+RelativeSure@users.noreply.github.com>
2024-03-09 21:03:25 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -89,26 +89,20 @@ return {
     saturation = 0.9,
     brightness = 0.8,
   },
-  tab_bar_style = {
-    active_tab_left = wezterm.format {
-      { Background = { Color = '#0b0022' } },
-      { Foreground = { Color = '#2b2042' } },
-      { Text = SOLID_LEFT_ARROW },
-    },
-    active_tab_right = wezterm.format {
-      { Background = { Color = '#0b0022' } },
-      { Foreground = { Color = '#2b2042' } },
-      { Text = SOLID_RIGHT_ARROW },
-    },
-    inactive_tab_left = wezterm.format {
-      { Background = { Color = '#0b0022' } },
-      { Foreground = { Color = '#1b1032' } },
-      { Text = SOLID_LEFT_ARROW },
-    },
-    inactive_tab_right = wezterm.format {
-      { Background = { Color = '#0b0022' } },
-      { Foreground = { Color = '#1b1032' } },
-      { Text = SOLID_RIGHT_ARROW },
+  keys = {
+    {
+      key = ',',
+      mods = 'CMD',
+      action = act.SpawnCommandInNewTab {
+        cwd = os.getenv('WEZTERM_CONFIG_DIR'),
+        set_environment_variables = {
+          TERM = 'screen-256color',
+        },
+        args = {
+          '/usr/local/bin/nvim',
+          os.getenv('WEZTERM_CONFIG_FILE'),
+        },
+      },
     },
   }
 }
