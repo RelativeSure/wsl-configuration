89e3108165278938d3586e35d0f9a0ca024ced30
RelativeSure <mail@rasmusj.dk>
2024-03-10 21:27:08 +0100

    updates

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,18 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+  source (jump shell fish | psub)
+end
+
+function starship_transient_prompt_func
+  starship module character
+end
+function starship_transient_rprompt_func
+  starship module time
 end
 starship init fish | source
+enable_transience
+
+set -gx GPG_TTY (tty)
 
 # pnpm
 set -gx PNPM_HOME "/home/rasmus/.local/share/pnpm"
@@ -22,3 +33,23 @@ fish_add_path $HOME/.cargo/bin
 set PATH $PATH /home/rasmus/.local/bin
 
 set VIRTUAL_ENV /home/rasmus/.venvs/lvim
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+set -gx MAMBA_EXE "/home/rasmus/.micromamba/bin/micromamba"
+set -gx MAMBA_ROOT_PREFIX "/home/rasmus/micromamba"
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/rasmus/micromamba/bin/conda
+    eval /home/rasmus/micromamba/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/rasmus/micromamba/etc/fish/conf.d/conda.fish"
+        . "/home/rasmus/micromamba/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/rasmus/micromamba/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
